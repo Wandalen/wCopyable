07d1b09dbe108adecfc0e2d06ffdda1413fa15f3
wandalen <wandalen.me@gmail.com>
2021-03-26 18:55:42 +0200

    .

--- proto/wtools/abase/l7_mixin/Copyable.s
+++ proto/wtools/abase/l7_mixin/Copyable.s
@@ -35,7 +35,7 @@ if( typeof module !== 'undefined' )
 
 const _global = _global_;
 const _ = _global_.wTools;
-var _ObjectHasOwnProperty = Object.hasOwnProperty;
+const _ObjectHasOwnProperty = Object.hasOwnProperty;
 
 _.assert( !!_._cloner );
 
